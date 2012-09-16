ac5d05083fa571c2ca9e81b2aca0fbf7b21172c8
Jignesh Malkan <jnmalkan@gmail.com>
2012-09-15 21:33:24 -0400

    added TodoService and changed method signature for findAll()

--- src/main/java/com/jiggy/sample/controller/TodosController.java
+++ src/main/java/com/jiggy/sample/controller/TodosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jiggy.sample.todo.Todo;
+import com.jiggy.sample.todo.TodoService;
 
 /**
  * Handles requests for the application todos request.
@@ -20,13 +22,37 @@ import com.jiggy.sample.todo.Todo;
 @Controller
 public class TodosController {
   private static final Logger logger = LoggerFactory.getLogger(TodosController.class);
+  
+  private TodoService todoService;
+  
+  /**
+   * Creates a new instance of com.jiggy.sample.controller.TodosController.java and Performs Initialization
+   */
+  public TodosController() {
+    super();
+  }
+  
+  /**
+   * Creates a new instance of com.jiggy.sample.controller.TodosController.java and Performs Initialization
+   */
+  @Autowired
+  public TodosController(TodoService todoService) {
+    super();
+    this.todoService = todoService;
+  }
 
   @ResponseBody
   @RequestMapping(method = RequestMethod.GET)
-  public Todo findAll() {
+  public List<Todo> findAll() {
     logger.info("findAll");
+    List<Todo> todos = new ArrayList<Todo>();
     
-    return new Todo(new Long(1), "wake up");
+    //List<Todo> todos = this.todoService.findAll();
+    todos.add(new Todo(new Long(1), "wake up"));
+    todos.add(new Todo(new Long(2), "do dishes"));
+    todos.add(new Todo(new Long(3), "take out trash"));
+    
+    return todos;
   }
   
   @ResponseBody
@@ -45,7 +71,6 @@ public class TodosController {
     
     todos.add(new Todo(new Long(1), "wake up"));
     todos.add(new Todo(new Long(2), "do dishes"));
-    todos.add(new Todo(new Long(3), "take out trash"));
     
     return todos;
   }
