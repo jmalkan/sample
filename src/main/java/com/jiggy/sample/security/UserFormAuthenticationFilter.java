e20bfbb849f981fb4c0a49ef140316a3c2ed79c4
Jignesh Malkan <jnmalkan@gmail.com>
2012-09-16 22:02:05 -0400

    code clean up and setting UserProfile in Session on Successful login.

--- src/main/java/com/jiggy/sample/security/UserFormAuthenticationFilter.java
+++ src/main/java/com/jiggy/sample/security/UserFormAuthenticationFilter.java
@@ -1,12 +1,7 @@
 package com.jiggy.sample.security;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.ws.rs.core.Response.Status;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -15,6 +10,7 @@ import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Extension of the basic forms authentication filter which supports: -forcing redirect to change password screen if user is marked as such -default
@@ -28,47 +24,34 @@ import org.slf4j.LoggerFactory;
 public class UserFormAuthenticationFilter extends FormAuthenticationFilter {
   private static final Logger logger = LoggerFactory.getLogger(UserFormAuthenticationFilter.class);
   
-//  private String loginFormUrl;
-//  
-//  public UserFormAuthenticationFilter() {
-//    this.loginFormUrl = "/login.html";
-//    setLoginUrl("/login");
-//  }
-//  
-//  public String getLoginFormUrl() {
-//    return this.loginFormUrl;
-//  }
-//  
-//  public void setLoginFormUrl(String loginFormUrl) {
-//    this.loginFormUrl = loginFormUrl;
-//  }
+  @Autowired
+  private UserService userService;
   
   @Override
   protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
     logger.info("onLoginSuccess");
+    
+    Object primaryPrincipal = subject.getPrincipals().getPrimaryPrincipal();
+    
+    if (primaryPrincipal instanceof UserPrincipal) {
+      UserPrincipal userPrincipal = (UserPrincipal) primaryPrincipal;
+      User user = this.userService.findById(userPrincipal.getId());
+      UserProfile userProfile = new UserProfile(user);
+      
+      SessionUtil.setUserProfile(userProfile);
+    }
     return super.onLoginSuccess(token, subject, request, response);
   }
   
   @Override
   protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
     logger.info("onLoginFailure");
-//    try {
-//      Map<String, String> params = new HashMap<String, String>();
-//      params.put("error", "true");
-//      WebUtils.issueRedirect(request, response, loginFormUrl, params);
-//      return false;
-//    } catch (IOException ex) {
-      return super.onLoginFailure(token, e, request, response);
-//    }
+    return super.onLoginFailure(token, e, request, response);
   }
   
   @Override
   protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
     logger.info("onAccessDenied WebUtils.toHttp(request).getRequestURL()= {}", WebUtils.toHttp(request).getRequestURL());
-//    if (!isLoginRequest(request, response) && !isLoginSubmission(request, response)) {
-//      WebUtils.toHttp(response).setStatus(Status.UNAUTHORIZED.getStatusCode());
-//      return false;
-//    }
     
     return super.onAccessDenied(request, response);
   }
